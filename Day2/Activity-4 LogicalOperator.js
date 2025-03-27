d043b7f2a67b7987383022f8f3a98d20db362b86
prerna <khannaprerna030@gmail.com>
2025-03-27 13:08:17 +0530

    file updated

--- Day2/Activity-4 LogicalOperator.js
+++ Day2/Activity-4 LogicalOperator.js
@@ -1,25 +1,15 @@
 // Task 11: && operator ka use karke do conditions ko combine karo aur result console par log karo
-let a=5;
-let b=6;
-let c=4;
-if(b>a && c>b) {
-console.log("b and c is greator");
-}else{
-    console.log("Condition is not met");
-}
+let a=17;
+let isValid = false;
+console.log((a>=18 || isValid),"Candidate is not eligible to vote");
 
 // Task 12: || operator ka use karke do conditons ko combine karo aur result console par log karo
 
 let age =17;
 let isEligible = false;
-let canVote = (age>=18 || isEligible);
-console.log("Candidate is eligible to vote");
+console.log((age>=18 || isEligible),"Candidate is eligible to vote");
 
 // Task 13: ! operator ka use karke condition ko negate kro aur result console par log karo
 
-let Age=5;
-if(!Age){
-    console.log("Age is:", Age);
-}else{
-    console.log("Valid age is:",Age);
-}
+let isAdult = true;
+console.log("Not eligible:",!isAdult);
--- Day2/Activity-5 TernaryOperator.js
+++ Day2/Activity-5 TernaryOperator.js
@@ -1,5 +1,4 @@
 // Task 14: Ternary operator ka use karke check karo ki koi number positive hai ya negative aur result console par log karo
 
 let a=-4;
-let result = (a>0) ? "Positive number" : "Negative Number";
-console.log(result);
+console.log((a>0) ? "Positive number" : "Negative Number");
